74954f21df9101c7d97e88394e47a4a2ad47779b
codefreak <harshsaid558@gmail.com>
2023-05-17 15:30:43 +0530

    mess model created

---
+++ model/messModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const mess = mongoose.Schema({
+    mess_name: {
+        type: String,
+        default: "",
+        required: [true, 'Please add your name'],
+    },
+    mess_contact: {
+        type: String,   
+        default: "",     
+    },
+    mess_password: {
+        type: String,
+        required: [true, 'Please add your password'],
+    },
+    mess_address: {
+        type: String,
+        default: "",
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("messModel",  mess);
---
+++ routes/messRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const messModel = require("../model/messModel");
+
+
+
+module.exports = router;
